a69b3d1de4d877b9c511f467ed1ac39aaaf01bdb
mierzvoj <mierzvoj@gmail.com>
2022-06-28 21:41:45 +0200

    tables changed again

--- src/pages/Users/UsersList/UsersList.tsx
+++ src/pages/Users/UsersList/UsersList.tsx
@@ -76,7 +76,7 @@ const UsersList = () => {
                 <TableCell className="header">
                   List of potential passengers
                 </TableCell>
-                <TableCell className="header">Add </TableCell>
+                <TableCell className="header">Add passengers</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
